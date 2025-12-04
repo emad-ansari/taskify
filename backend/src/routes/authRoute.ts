54d3077199eda5ccafe19a6947e6e3a5d4319124
92Mohammad <m.e89571828@gmail.com>
2025-12-04 18:48:12 +0530

    feat: created register and login routes

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -2,54 +2,63 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { User } from "../model/user";
-
+import { generateToken } from "../middleware/auth";
 
 const router = express.Router();
 
-
 router.post("/register", async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
+	try {
+		const { username, email, password } = req.body;
 
-    const exists = await User.findOne({ email });
-    if (exists) return res.status(400).json({ message: "Email already in use" });
+		const exists = await User.findOne({ email });
+		if (exists)
+			return res.status(400).json({ message: "Email already in use" });
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+		const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = await User.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
+		const user = await User.create({
+			username,
+			email,
+			password: hashedPassword,
+		});
 
-    res.json({ message: "User registered successfully", user });
-  } catch (err) {
-    res.status(500).json({ message: "Server error" });
-  }
+		const token = generateToken(user._id.toString(), user.email as string);
+
+		res.json({
+			message: "User registered successfully",
+			data: { token, username: user.username },
+		});
+	} catch (err) {
+		res.status(500).json({ message: "Server error" });
+	}
 });
 
 router.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
+	try {
+		const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
-    if (!user || !user.password) return res.status(400).json({ message: "Invalid email or password" });
+		const user = await User.findOne({ email });
+		if (!user || !user.password)
+			return res
+				.status(400)
+				.json({ message: "Invalid email or password" });
 
-    const valid = await bcrypt.compare(password, user.password);
-    if (!valid) return res.status(400).json({ message: "Invalid email or password" });
+		const valid = await bcrypt.compare(password, user.password);
+		if (!valid)
+			return res
+				.status(400)
+				.json({ message: "Invalid email or password" });
 
-    // create token
-    const token = jwt.sign(
-      { id: user._id, email: user.email },
-      process.env.JWT_SECRET!,
-      { expiresIn: "7d" }
-    );
+		const token = generateToken(user._id.toString(), user.email as string);
 
-    res.json({ message: "Login success", token });
-  } catch (err) {
-    res.status(500).json({ message: "Server error" });
-  }
+		res.json({
+			message: "Login successfully",
+			data: { token, username: user.username },
+		});
+	} catch (err) {
+		console.log("Error: ", err);
+		res.status(500).json({ message: "Server error" });
+	}
 });
-
 
 export default router;
