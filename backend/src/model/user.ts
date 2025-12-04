375a02a39bfc9af0a6020a6f220e96bd2c71f370
92Mohammad <m.e89571828@gmail.com>
2025-12-04 18:41:07 +0530

    feat: added username field

--- backend/src/model/user.ts
+++ backend/src/model/user.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    name: String,
+    username: String,
     email: { type: String, unique: true },
     password: String,
   },
