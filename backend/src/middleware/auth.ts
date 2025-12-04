a10bb290c113f88b0cfeab9009603a997760bd6d
92Mohammad <m.e89571828@gmail.com>
2025-12-04 18:38:29 +0530

    feat: added generate Token method

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,18 +1,28 @@
 import jwt from "jsonwebtoken";
-import { type Request,type Response,type NextFunction } from "express";
+import { type Request, type Response, type NextFunction } from "express";
+import type { ObjectId, Types } from "mongoose";
 
 export const auth = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const token = req.headers.authorization?.split(" ")[1];
+	try {
+		const token = req.headers.authorization?.split(" ")[1];
 
-    if (!token) return res.status(401).json({ message: "Unauthorized" });
+		if (!token) return res.status(401).json({ message: "Unauthorized" });
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
-    // @ts-ignore
-    req.user = decoded
+		const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+		// @ts-ignore
+		req.user = decoded;
 
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
+		next();
+	} catch (error) {
+		return res.status(401).json({ message: "Invalid token" });
+	}
+};
+
+export const generateToken = (userId: string, email: string) => {
+	const token = jwt.sign(
+		{ id: userId, email: email },
+		process.env.JWT_SECRET!,
+		{ expiresIn: "7d" }
+	);
+	return token;
 };
