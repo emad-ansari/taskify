eddf1cf71d9cfbd24f177ad989d1456b782d340e
92Mohammad <m.e89571828@gmail.com>
2025-12-06 19:00:34 +0530

    removed signup and login button

--- frontend/src/components/common/header.tsx
+++ frontend/src/components/common/header.tsx
@@ -1,69 +1,76 @@
-import { Sparkles, Bell, User, CircleCheckBig } from "lucide-react";
-import { Link, useLocation } from "react-router-dom";
+import { User, CircleCheckBig } from "lucide-react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useAuth } from "@/hooks/useAuth";
 import { Button } from "@/components/ui/button";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
 export const Header = () => {
-  const location = useLocation();
-  
-  const isActive = (path: string) => location.pathname === path;
-  
-  return (
-    <header className="border-b bg-card">
-      <div className="container mx-auto px-6 py-4 flex items-center justify-between">
-        <div className="flex items-center gap-8">
-          <Link to="/" className="flex items-center gap-2 ">
-            <CircleCheckBig strokeWidth={3} className="w-10 h-10 text-primary "/>
-            <span className="font-bold">TaskFlow</span>
-          </Link>
-          
-          <nav className="hidden md:flex items-center gap-6">
-            <Link
-              to="/"
-              className={`text-sm font-medium transition-colors hover:text-primary ${
-                isActive("/") ? "text-primary" : "text-muted-foreground"
-              }`}
-            >
-              Dashboard
-            </Link>
-            <Link
-              to="/tasks"
-              className={`text-sm font-medium transition-colors hover:text-primary ${
-                isActive("/tasks") ? "text-primary" : "text-muted-foreground"
-              }`}
-            >
-              Tasks
-            </Link>
-          </nav>
-        </div>
-        
-        <div className="flex items-center gap-3">
-          <Button variant="ghost" size="icon" className="relative rounded-full cursor-pointer">
-            <Bell className="h-5 w-5" />
-            <span className="absolute top-1 right-1 h-2 w-2 rounded-full bg-destructive" />
-          </Button>
-          
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" size="icon" className = "cursor-pointer rounded-full">
-                <div className="h-8 w-8 rounded-full bg-primary/10 flex items-center justify-center">
-                  <User className="h-4 w-4 text-primary" />
-                </div>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuItem>Profile</DropdownMenuItem>
-              <DropdownMenuItem>Settings</DropdownMenuItem>
-              <DropdownMenuItem>Log out</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
-      </div>
-    </header>
-  );
+	const location = useLocation();
+
+	const isActive = (path: string) => location.pathname === path;
+
+	return (
+		<header className="border-b bg-card">
+			<div className="container mx-auto px-6 py-4 flex items-center justify-between">
+				<div className="flex items-center gap-8">
+					<Link to="/" className="flex items-center gap-2 ">
+						<CircleCheckBig
+							strokeWidth={3}
+							className="w-9 h-9 text-primary "
+						/>
+						<span className="font-bold">TaskFlow</span>
+					</Link>
+
+					<nav className="hidden md:flex items-center gap-6">
+						<Link
+							to="/dashboard"
+							className={`text-sm font-medium transition-colors hover:text-primary ${
+								isActive("/")
+									? "text-primary"
+									: "text-muted-foreground"
+							}`}
+						>
+							Dashboard
+						</Link>
+						<Link
+							to="/tasks"
+							className={`text-sm font-medium transition-colors hover:text-primary ${
+								isActive("/tasks")
+									? "text-primary"
+									: "text-muted-foreground"
+							}`}
+						>
+							Tasks
+						</Link>
+					</nav>
+				</div>
+
+				<div className="flex items-center gap-3">
+					<DropdownMenu>
+						<DropdownMenuTrigger asChild>
+							<Button
+								variant="ghost"
+								size="icon"
+								className="cursor-pointer rounded-full"
+							>
+								<div className="h-8 w-8 rounded-full bg-primary/10 flex items-center justify-center">
+									<User className="h-4 w-4 text-primary" />
+								</div>
+							</Button>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent align="end">
+							<DropdownMenuItem>Profile</DropdownMenuItem>
+							<DropdownMenuItem>Settings</DropdownMenuItem>
+							<DropdownMenuItem>Log out</DropdownMenuItem>
+						</DropdownMenuContent>
+					</DropdownMenu>
+				</div>
+			</div>
+		</header>
+	);
 };
